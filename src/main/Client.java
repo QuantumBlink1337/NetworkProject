2095e17281737901d5546682eb83e34b6dbd309c
Matt <mm7f5@umsystem.edu>
2024-02-19 21:14:12 -0600

    added exception catch for when server is down

--- src/main/Client.java
+++ src/main/Client.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
-import java.net.Socket;
+import java.net.*;
 import java.util.Scanner;
 
 public class Client {
@@ -74,14 +74,21 @@ public class Client {
     }
 
     public void startConnection(String ip, int port) throws IOException {
-        clientSocket = new Socket(ip, port);
+        try {
+            clientSocket = new Socket(ip, port);
+        }
+        catch (ConnectException e) {
+            System.out.println("There was a problem connecting to the server");
+            e.printStackTrace();
+            System.exit(1);
+        }
         out = new PrintWriter(clientSocket.getOutputStream(), true);
         in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
         connected = true;
     }
 
     public void inputLoop() throws IOException {
-        System.out.println("Chatroom main.Client v. 0.0.1");
+        System.out.println("Chatroom Client version 1");
         try {
             startConnection("127.0.0.1", 1060);
 
